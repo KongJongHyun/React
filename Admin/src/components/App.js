c5c3ffb4ab50a715d6858a6dbbbefef70f42bafe
kong3 <kong@kinterch.com>
2019-11-05 14:07:03 +0900

    Administrator Page

---
+++ Admin/src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import ResponsiveDrawer from './ResponsiveDrawer';
+import Copyright from './Signln';
+
+
+  const App = () => (
+    <>
+    {/*}
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  */}
+    {/*<ResponsiveDrawer></ResponsiveDrawer>*/}
+    <Copyright />
+    </>
+  );
+
+export default App;
