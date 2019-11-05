155f073220827199c5fe010d36905abe88c8303a
kong3 <kong@kinterch.com>
2019-11-05 14:22:39 +0900

    login file

---
+++ login/src/Menu/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+      },
+    }));
+function Main() {
+    const classes = useStyles();
+    return(
+        <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+        Menu
+        </Typography>
+        </main>
+    )
+}
+
+export default Main;
---
+++ login/src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+import Copyright from './Signln';
+
+
+  const App = () => (
+    <>
+    <Copyright />
+    </>
+  );
+
+export default App;
